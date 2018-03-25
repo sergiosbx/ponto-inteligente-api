303f2f118d1311910a92dc39ed8400e8e809c8df
Sérgio Ferreira Filho <sergio.ferreira.filho@gmail.com>
2018-03-24 21:49:52 -0300

    Subindo novos serviços e mudando controle de segurança da aplicação

--- src/main/resources/db/migration/mysql/V1__init.sql
+++ src/main/resources/db/migration/mysql/V1__init.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `empresa` (
+CREATE TABLE IF NOT EXISTS `empresa` (
   `id` bigint(20) NOT NULL,
   `cnpj` varchar(255) NOT NULL,
   `data_atualizacao` datetime NOT NULL,
@@ -6,13 +6,10 @@ CREATE TABLE `empresa` (
   `razao_social` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-ALTER TABLE `empresa`
-  ADD PRIMARY KEY (`id`);
-  
-ALTER TABLE `empresa`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `empresa` ADD PRIMARY KEY (`id`);  
+ALTER TABLE `empresa` MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
 
-CREATE TABLE `funcionario` (
+CREATE TABLE IF NOT EXISTS `funcionario` (
   `id` bigint(20) NOT NULL,
   `cpf` varchar(255) NOT NULL,
   `data_atualizacao` datetime NOT NULL,
@@ -27,7 +24,16 @@ CREATE TABLE `funcionario` (
   `empresa_id` bigint(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `lancamento` (
+ALTER TABLE `funcionario`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK4cm1kg523jlopyexjbmi6y54j` (`empresa_id`);
+
+ALTER TABLE `funcionario` MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `funcionario`
+  ADD CONSTRAINT `FK4cm1kg523jlopyexjbmi6y54j` FOREIGN KEY (`empresa_id`) REFERENCES `empresa` (`id`);
+
+CREATE TABLE IF NOT EXISTS `lancamento` (
   `id` bigint(20) NOT NULL,
   `data` datetime NOT NULL,
   `data_atualizacao` datetime NOT NULL,
@@ -38,46 +44,12 @@ CREATE TABLE `lancamento` (
   `funcionario_id` bigint(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
-/* 
---
--- Indexes for table `funcionario`
---
-ALTER TABLE `funcionario`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `FK4cm1kg523jlopyexjbmi6y54j` (`empresa_id`);
-
---
--- Indexes for table `lancamento`
---
 ALTER TABLE `lancamento`
   ADD PRIMARY KEY (`id`),
   ADD KEY `FK46i4k5vl8wah7feutye9kbpi4` (`funcionario_id`);
-
-
---
--- AUTO_INCREMENT for table `funcionario`
---
-ALTER TABLE `funcionario`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `lancamento`
---
+  
 ALTER TABLE `lancamento`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
---
--- Constraints for dumped tables
---
 
---
--- Constraints for table `funcionario`
---
-ALTER TABLE `funcionario`
-  ADD CONSTRAINT `FK4cm1kg523jlopyexjbmi6y54j` FOREIGN KEY (`empresa_id`) REFERENCES `empresa` (`id`);
-
---
--- Constraints for table `lancamento`
---
 ALTER TABLE `lancamento`
   ADD CONSTRAINT `FK46i4k5vl8wah7feutye9kbpi4` FOREIGN KEY (`funcionario_id`) REFERENCES `funcionario` (`id`);
-  */
