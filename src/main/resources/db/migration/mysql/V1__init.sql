dc6d06a895c54ad2ea286f2ebf22005d3af393ca
Sérgio Ferreira Filho <sergio.ferreira.filho@gmail.com>
2018-04-02 11:06:36 -0300

    ajustando input inicial do banco

--- src/main/resources/db/migration/mysql/V1__init.sql
+++ src/main/resources/db/migration/mysql/V1__init.sql
@@ -25,13 +25,10 @@ CREATE TABLE IF NOT EXISTS `funcionario` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 ALTER TABLE `funcionario`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `FK4cm1kg523jlopyexjbmi6y54j` (`empresa_id`);
+  ADD PRIMARY KEY (`id`);
 
 ALTER TABLE `funcionario` MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
 
-ALTER TABLE `funcionario`
-  ADD CONSTRAINT `FK4cm1kg523jlopyexjbmi6y54j` FOREIGN KEY (`empresa_id`) REFERENCES `empresa` (`id`);
 
 CREATE TABLE IF NOT EXISTS `lancamento` (
   `id` bigint(20) NOT NULL,
@@ -45,11 +42,7 @@ CREATE TABLE IF NOT EXISTS `lancamento` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 ALTER TABLE `lancamento`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `FK46i4k5vl8wah7feutye9kbpi4` (`funcionario_id`);
+  ADD PRIMARY KEY (`id`);
   
 ALTER TABLE `lancamento`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
-
-ALTER TABLE `lancamento`
-  ADD CONSTRAINT `FK46i4k5vl8wah7feutye9kbpi4` FOREIGN KEY (`funcionario_id`) REFERENCES `funcionario` (`id`);
