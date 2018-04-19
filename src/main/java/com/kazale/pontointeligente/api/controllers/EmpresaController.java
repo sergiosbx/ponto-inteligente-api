ac237fb2f5189d925e82de3d62cf308f446eb1b7
Sérgio Ferreira Filho <sergio.ferreira.filho@gmail.com>
2018-04-18 21:59:10 -0300

    Empresa, FUncionario e Lancamento

---
+++ src/main/java/com/kazale/pontointeligente/api/controllers/EmpresaController.java
@@ -0,0 +1,56 @@
+package com.kazale.pontointeligente.api.controllers;
+
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kazale.pontointeligente.api.dtos.EmpresaDto;
+import com.kazale.pontointeligente.api.entities.Empresa;
+import com.kazale.pontointeligente.api.response.Response;
+import com.kazale.pontointeligente.api.services.EmpresaService;
+
+@RestController
+@RequestMapping("/api/empresas")
+@CrossOrigin(origins = "*")
+public class EmpresaController {
+	
+	private static final Logger log = LoggerFactory.getLogger(EmpresaController.class);
+
+	public EmpresaController() {}
+	
+	@Autowired
+	private EmpresaService empresaService;
+	
+	@GetMapping(value = "/cnpj/{cnpj}")
+	public ResponseEntity<Response<EmpresaDto>> buscarPorCnpj(@PathVariable("cnpj") String cnpj) {
+		log.info("Buscando empresa por CNPJ: {}", cnpj);
+		Response<EmpresaDto> response = new Response<EmpresaDto>();
+		Optional<Empresa> empresa = empresaService.buscarPorCnpj(cnpj);
+		
+		if (!empresa.isPresent()) {
+			log.info("Empresa não encontrada para o CNPJ: {}", cnpj);
+			response.getErrors().add("Empresa não encontrada para o CNPJ " + cnpj);
+			return ResponseEntity.badRequest().body(response);
+		}
+		
+		response.setData(this.converterEmpresaDto(empresa.get()));
+		return ResponseEntity.ok(response);
+		
+	}
+
+	private EmpresaDto converterEmpresaDto(Empresa empresa) {
+		EmpresaDto empresaDto = new EmpresaDto();
+		empresaDto.setId(empresa.getId());
+		empresaDto.setCnpj(empresa.getCnpj());
+		empresaDto.setRazaoSocial(empresa.getRazaoSocial());
+		return empresaDto;
+	}
+}
